5c5347bbc0438103abea999e5b12e9932a2a48b1
changhui98 <net_0102@naver.com>
2025-11-25 23:26:24 +0900

    Fix : yml ddl-auto update

--- user-service/src/main/java/com/gbg/userservice/presentation/controller/UserController.java
+++ user-service/src/main/java/com/gbg/userservice/presentation/controller/UserController.java
@@ -6,8 +6,8 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +19,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/sign")
-    public ResponseEntity<UUID> createUser(UserSignRequest userSignRequest) {
+    public ResponseEntity<UUID> createUser(@RequestBody UserSignRequest userSignRequest) {
 
         UUID uuid = userService.createUser(userSignRequest);
 
